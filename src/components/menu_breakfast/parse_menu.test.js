ab7fae3cbe7a0363888cf2fb3896138b651b6d70
DrakeDatBoi <94668463+DrakeDatBoi@users.noreply.github.com>
2021-12-13 01:26:09 -0600

    Add files via upload

---
+++ src/components/menu_breakfast/parse_menu.test.js
@@ -0,0 +1,11 @@
+// const request = require('supertest');
+
+// const log = require('../server/log');
+// log.setEnabled(false);
+
+const getFormattedList = require("./parse_menu");
+
+// test("Returns list of menu items", () => {
+//     new_list = getFormattedList()
+//     expect(new_list.length).toBe(27);
+// });
